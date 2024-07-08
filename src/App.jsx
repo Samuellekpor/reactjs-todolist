86e73dfaf8b071721f3f35035c18298cbe33685a
unknown <samuel.lekpor@gmail.com>
2024-07-08 22:10:29 +0000

    Added the logic to edit a todo

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
 
 function App() {
-
   const [todos, setTodos] = useState([]);
+  const [todoValue, setTodoValue] = useState("");
 
   function handleAddTodos(newTodo) {
     setTodos([...todos, newTodo]);
@@ -15,12 +15,26 @@ function App() {
     setTodos(newTodosList);
   }
 
+  function handleEditTodo(index) {
+    const valueTObeEdited = todos[index];
+    setTodoValue(valueTObeEdited);
+    handleDeleteTodo(index);
+  }
+
   return (
     <main>
-      <TodoInput handleAddTodos={handleAddTodos} />
-      <TodoList handleDeleteTodo={handleDeleteTodo} todos={todos}/>
+      <TodoInput
+        todoValue={todoValue}
+        setTodoValue={setTodoValue}
+        handleAddTodos={handleAddTodos}
+      />
+      <TodoList
+        handleDeleteTodo={handleDeleteTodo}
+        handleEditTodo={handleEditTodo}
+        todos={todos}
+      />
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
